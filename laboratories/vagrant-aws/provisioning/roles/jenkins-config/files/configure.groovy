f77229a4ea2ddfa6a2ee9a846de436172374c74d
Vish <savishy.pv@gmail.com>
2017-03-27 09:26:52 +0000

    laboratory WIP - added role to configure jenkins users --- WIP - added functionality to execute CLI commands

---
+++ laboratories/vagrant-aws/provisioning/roles/jenkins-config/files/configure.groovy
@@ -0,0 +1,16 @@
+//this script enables security on a fresh Jenkins machine
+//and creates an initial admin user.
+//credit: https://gist.github.com/hayderimran7/50cb1244cc1e856873a4
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin","Alohomora")
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new GlobalMatrixAuthorizationStrategy()
+strategy.add(Jenkins.ADMINISTER, "myUSERNAME")
+instance.setAuthorizationStrategy(strategy)
+instance.save()
